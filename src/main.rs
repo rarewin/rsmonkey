b195d9e85eb933aa1a9a2b996a024f47b6d450a4
Katsuki Kobayashi <rare@tirasweel.org>
2018-11-28 01:46:10 +0900

    add enum for tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+enum TokenType {
+    Ileegal,
+    EoF,
+
+    Ident,
+    Int,
+
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+
+    LT,
+    GT,
+
+    Eq,
+    NotEq,
+
+    Comma,
+    Semicolon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
 fn main() {
     println!("Hello, world!");
 }

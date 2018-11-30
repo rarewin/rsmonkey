01219ba1fd4d706e3ac7a3d60b4d13d6b43ee046
Katsuki Kobayashi <rare@tirasweel.org>
2018-11-30 21:38:13 +0900

    implement a very early version of REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::io;
+use std::io::Write;
+
 #[derive(Debug)]
 struct Token {
     token_type: TokenType,
@@ -311,6 +315,36 @@ fn lookup_ident(s: &str) -> TokenType {
     }
 }
 
+fn read_input() -> io::Result<()> {
+    let mut input = String::new();
+    let username = env::vars().find(|x| x.0 == "USER").unwrap_or_default().1;
+
+    println!(
+        "Hello {}! This is the `RS'Monkey programming language!",
+        username
+    );
+    println!("Feel free to type in commands");
+
+    while {
+        input.clear();
+        print!(">> ");
+        io::stdout().flush();
+        io::stdin().read_line(&mut input)?;
+
+        let mut l = Lexer::new(input.to_string());
+
+        while {
+            let t = l.next_token();
+            println!("{:?}", t);
+            t.token_type != TokenType::EoF
+        } {}
+
+        !input.is_empty()
+    } {}
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    read_input();
 }

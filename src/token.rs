b8c29267c78cae1ff4b3a2d264765481dcfa535c
Katsuki Kobayashi <rare@tirasweel.org>
2018-12-01 21:51:32 +0900

    start to write a documentation

--- src/token.rs
+++ src/token.rs
@@ -1,57 +1,123 @@
+/// struct for tokens
 #[derive(Debug, PartialEq)]
 pub struct Token {
+    /// type of token
     token_type: TokenType,
+    /// literal string
     literal: String,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, l: &str) -> Token {
-        let literal = l.to_string();
+    /// constructor for token
+    ///
+    /// # Arguments
+    ///
+    /// * `token_type` - type of token
+    /// * `literal_str` - literal string
+    ///
+    /// # Return value
+    ///
+    /// return a new Token
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// extern crate rsmonkey;
+    /// use rsmonkey::token::{TokenType, Token};
+    ///
+    /// let token = Token::new(TokenType::Int, "4");
+    /// let token = Token::new(TokenType::Ident, "hoge");
+    /// ```
+    pub fn new(token_type: TokenType, literal_str: &str) -> Token {
+        let literal = literal_str.to_string();
         Token {
             token_type,
             literal,
         }
     }
 
+    /// check if EoF or not
+    ///
+    /// # Return value
+    ///
+    /// return true if Token is EoF, otherwise false
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// extern crate rsmonkey;
+    /// use rsmonkey::token::{TokenType, Token};
+    ///
+    /// assert!(Token::new(TokenType::EoF, "\0").is_eof());
+    /// assert!(!Token::new(TokenType::Int, "23").is_eof());
+    /// ```
     pub fn is_eof(&self) -> bool {
         self.token_type == TokenType::EoF
     }
 }
 
+/// Token type
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
+    /// illegal token
     Illegal,
+    /// End of File
     EoF,
 
+    /// identifier
     Ident,
+    /// integer
     Int,
 
+    /// assign `=`
     Assign,
+    /// plus `+`
     Plus,
+    /// minus `-`
     Minus,
+    /// bang `!`
     Bang,
+    /// asterisk `*`
     Asterisk,
+    /// slash `/`
     Slash,
 
+    /// less than `<`
     LT,
+    /// greater than `>`
     GT,
 
+    /// equal `==`
     Eq,
+    /// not equal `!=`
     NotEq,
 
+    /// comma `,`
     Comma,
+    /// semicolon `;`
     Semicolon,
 
+    /// left parenthesis `(`
     LParen,
+    /// right parenthesis `)`
     RParen,
+    /// left curly brace `{`
     LBrace,
+    /// right curly brace `}`
     RBrace,
 
+    /// function `fn`
     Function,
+    /// let
     Let,
+    /// true
     True,
+    /// false
     False,
+    /// if
     If,
+    /// else
     Else,
+    /// return
     Return,
 }

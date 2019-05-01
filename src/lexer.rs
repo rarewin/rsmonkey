078e94b65c619e48366bd4e9e393537056ef32af
Katsuki Kobayashi <rare@tirasweel.org>
2019-05-01 18:29:12 +0900

    refactor lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,29 +9,18 @@ pub struct Lexer {
 }
 
 impl Lexer {
+    /// create new lexer
     pub fn new(input: String) -> Lexer {
-        let mut l = Lexer {
-            input: input,
+        let ch = input.as_bytes()[0] as char;
+        Lexer {
+            input,
             position: 0,
-            read_position: 0,
-            ch: '\0',
-        };
-        l.read_char();
-
-        return l;
+            read_position: 1,
+            ch,
+        }
     }
 
-    pub fn read_char(&mut self) {
-        self.ch = if self.read_position >= self.input.len() {
-            '\0'
-        } else {
-            self.input.as_bytes()[self.read_position] as char
-        };
-
-        self.position = self.read_position;
-        self.read_position += 1;
-    }
-
+    /// get next token
     pub fn next_token(&mut self) -> Token {
         self.skip_whitespace();
         let token = match self.ch {
@@ -66,64 +55,57 @@ impl Lexer {
             ',' => Token::new(TokenType::Comma, ","),
             ';' => Token::new(TokenType::Semicolon, ";"),
             '\0' => Token::new(TokenType::EoF, "EOF"),
-            '"' => Token::new(TokenType::StringToken, &self.read_string()),
-            _ => {
-                if is_letter(self.ch) {
-                    let p = self.position;
-                    while is_letter(self.ch) {
-                        self.read_char();
-                    }
-                    let v = &self.input[p..self.position].to_string();
-                    return Token::new(lookup_ident(&v), v);
-                } else if is_digit(self.ch) {
-                    let p = self.position;
-                    while is_digit(self.ch) {
-                        self.read_char();
-                    }
-                    return Token::new(TokenType::Int, &self.input[p..self.position].to_string());
-                } else {
-                    return Token::new(TokenType::Illegal, "illegal");
+            '"' => {
+                let p = self.read_position;
+                self.read_char();
+                while self.ch != '"' && self.read_position < self.input.len() {
+                    self.read_char();
                 }
+                Token::new(TokenType::StringToken, &self.input[p..self.position])
             }
+            'a'...'z' | 'A'...'Z' | '_' => {
+                let p = self.position;
+                while self.ch.is_ascii_alphabetic() || self.ch == '_' {
+                    self.read_char();
+                }
+                let v = &self.input[p..self.position];
+                return Token::new(lookup_ident(v), v); // don't need to read char more
+            }
+            '0'...'9' => {
+                let p = self.position;
+                while self.ch.is_ascii_digit() {
+                    self.read_char();
+                }
+                return Token::new(TokenType::Int, &self.input[p..self.position]); // don't need to read char more
+            }
+            _ => Token::new(TokenType::Illegal, "illegal"),
         };
         self.read_char();
 
-        return token;
+        token
     }
 
-    pub fn peek_char(&mut self) -> char {
+    fn read_char(&mut self) {
+        self.ch = self.peek_char();
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn peek_char(&mut self) -> char {
         if self.read_position >= self.input.len() {
-            return '\0';
+            '\0'
         } else {
-            return self.input.as_bytes()[self.read_position] as char;
+            self.input.as_bytes()[self.read_position] as char
         }
     }
 
-    pub fn skip_whitespace(&mut self) {
-        while self.ch == ' ' || self.ch == '\t' || self.ch == '\n' || self.ch == '\r' {
+    /// skip white spaces
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_ascii_whitespace() {
             self.read_char();
         }
     }
-
-    pub fn read_string(&mut self) -> String {
-        let mut ret = String::new();
-        self.read_char();
-        while self.ch != '"' && self.read_position < self.input.len() {
-            ret.push(self.ch);
-            self.read_char();
-        }
-        return ret;
-    }
-}
-
-/// Return true if ch is letter.
-fn is_letter(ch: char) -> bool {
-    return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_';
-}
-
-/// Retutn true if ch is digit.
-fn is_digit(ch: char) -> bool {
-    return '0' <= ch && ch <= '9';
 }
 
 /// lookup identifier
